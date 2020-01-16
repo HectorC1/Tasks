71fd84106be6ba154d55a9bf28cee89b874b37f9
HectorC1 <32409153+HectorC1@users.noreply.github.com>
2020-01-16 05:11:00 -0800

    Update bicycleLock.java

--- bicycleLock.java
+++ bicycleLock.java
@@ -6,10 +6,14 @@ the number value of a ring, and a "twist down" decreases it. For example, if
 the actual number shown is 1729 and the desired combination is 5714, write your 
 instructions in java like this: 
 Ring 1: Twist up 4 times 
+Ring 1: Twist up once
 Ring 3: Twist down once
+Ring 3: Twist down twice
+Ring 3: Twist up or down 3 times
+Ring 4: Twist down 2 times
+Ring 4: Twist down once
 Ring 4: Twist up or down 5 times
 
-Haven't completely finished, still need to shorten code and edit
 */
 import java.util.Scanner;
 import java.util.HashMap;
@@ -17,18 +21,15 @@ import java.util.*;
 
 
 class BicycleLock{
-    public static int[] lock = new int[]{1,7,1,4};
+    public static int[] lock = new int[]{1,7,2,9};
     public static int[] unlock = new int[]{5,7,1,4};
     
+    public static int twistOption;
+
     int currentRing = 0;
     
-    HashMap<Integer, Integer> mapNum = new HashMap<>();
+    
 
-    public void createMap(){
-        for(int i = 0; i < 10; i++){
-            mapNum.put(i,i);
-        }
-    }
     public void printLock(){
         for(int i = 0; i < lock.length; i++){
             System.out.print(lock[i]+" ");
@@ -47,22 +48,30 @@ class BicycleLock{
         
     public boolean lockEquals(int[] array1, int[] array2){
 
+        boolean equal = true;
         for(int i = 0; i < array1.length; i++){
-            if(array1[i] == array2[i]){
-                return true;
+            if(array1[i] != array2[i]){
+                equal = false; 
             }
-            else 
-            return false;
         }
-        return false;
+        return equal;
+        
     }
 
-   public boolean hasTwistUp(String twistUp){
-       if(twistUp.contains("Twist up") || twistUp.contains("twist up")){
-           return true;
-       }
-       else return false;
+    public boolean hasTwist(String twist){
+        if(twist.contains("Twist up") || twist.contains("twist up")){
+            twistOption = 1;
+            return true;
+        }
+
+        else if(twist.contains("Twist down") || twist.contains("twist down")){
+            twistOption = 2;
+            return true;
+        }
+         else return false;
+        
     }
+
 
     public boolean hasRing(String ring){
         if (ring.contains("ring") ||
@@ -72,59 +81,56 @@ class BicycleLock{
         else return false;
     }
 
-   public boolean hasTwistDown(String twistDown){
-    if(twistDown.contains("Twist down") || twistDown.contains("twist down")){
-        return true;
-    }
-    else return false;
-     }
+ 
 
-   public void turnCell1(int n){
+   public void turnRing(int ring, int times, int twistOption, int turnTimeString){
+       // turnTimeString is going to be 1 for once and 2 for twice
+       // twist option is 1 for up and 2 for down
+
+        if(turnTimeString == 1 || turnTimeString == 2){
+            times = 0;
+
+            for(int i = 0; i < turnTimeString; i++){
+
+                if(twistOption == 1 && (turnTimeString == 1 || turnTimeString == 2)){
+                    
+                    lock[ring] += 1;
     
-        for(int i = 0; i < n; i++){
-            
-           
-            lock[0] += 1;
-
-            if(lock[0] > 9){
-                lock[0] = 0;
+                if(lock[ring] > 9){
+                    lock[ring] = 0;
+                }   
             }
-            
-        }
-   }
-
-   public void turnCell2(int n){
-    for(int i = 0; i < n; i++){
-        lock[1] += 1;
-
-        if(lock[1] > 9){
-            lock[1] = 0;
-    }
+                else if(twistOption == 2 && (turnTimeString == 1 || turnTimeString == 2)){
+                    lock[ring] -= 1;
+                    if(lock[ring] < 0){
+                        lock[ring] = 9;
+                    }
     
-   }
-}
-   public void turnCell3(int n){
-    for(int i = 0; i < n; i++){
-        lock[2] += 1;
-
-        if(lock[2] > 9){
-            lock[2] = 0;
-    }
-}
-    }   
-    public void turnCell4(int n){
-        for(int i = 0; i < n; i++){
-            lock[3] += 1;
-
-            if(lock[3] > 9){
-                lock[3] = 0;
+                }
+            }
         }
-     }
-    }
-   public void currentRing(int ring){
 
+        for(int i = 0; i < times; i++){
+
+            if(twistOption == 1){
+                
+                lock[ring] += 1;
+
+            if(lock[ring] > 9){
+                lock[ring] = 0;
+            }   
+        }
+            else if(twistOption == 2){
+                lock[ring] -= 1;
+                if(lock[ring] < 0){
+                    lock[ring] = 9;
+                }
+
+            }
+          
+               
+        }
    }
-
    
    public void setRing(int currentRing){
        this.currentRing = currentRing;
@@ -134,15 +140,6 @@ class BicycleLock{
        return currentRing;
    }
 
-//    public void containsRing(String ring){
-//        if(ring.contains("Ring 1") ||
-//        ring.contains("Ring 2") ||
-//        ring.contains("Ring 3") ||
-//        ring.contains("Ring 4") 
-//        ){
-
-//        }
-//    }
 
    public void containsRing(String s){
        // check if string has a digit
@@ -153,6 +150,7 @@ class BicycleLock{
        int ringNum = 0;
        int firstNum = 0;
        int turnTimes = 0;
+       int turnTimeString = 0;
        String ringSub = s.substring(0, 7);
        String turnSub= s.substring(7, s.length());
        String turnSubString = s.substring(7, s.length()); // "once or twice"
@@ -160,52 +158,25 @@ class BicycleLock{
        char[] charArray = new char[s.length()];
 
        for(int i = 0; i < s.length(); i++){
-
-        //loop through substring then get the number
-
+        //loop through substring then get tw    he number
            ring += s.charAt(i);
            numbers += s.charAt(i);
         
 
-        
-        //    if (s.charAt(i) == (':')){
-        //    System.out.println("end");
-        //    break;
-        //    }
         }
 
-        System.out.println(ring);
-        if(hasRing(ring)){
-            System.out.println("has ring");
+        if(turnSub.contains("once")){
+            System.out.println("valid");
+            turnTimeString = 1;
         }
 
-        if(hasTwistUp(s)){
-               System.out.println("has twist up ");
-               
-               // changing the cell turnTimes incrementing by 1 each time
+        if(turnSub.contains("twice")){
+            System.out.println("valid");
 
-               }
-        
-
-        if(hasTwistDown(s)){
-            System.out.println("has twist down");
-         }
-        
-        if(ring.contains("ring 1")){
-
-            System.out.println(1);
-        }
-        else if(ring.equals("ring 2")){
-            System.out.println(2);
-        }
-         
-        else if(ring.equals("ring 3")){
-            System.out.println(3);
-        }
-        else if(ring.equals("ring 4")){
-            System.out.println(4);
+            turnTimeString = 2;
         }
 
+    
        for(int j = 0; j < ringSub.length(); j++){
             
         if(Character.isDigit(ringSub.charAt(j))){
@@ -225,50 +196,28 @@ class BicycleLock{
             turnTimes = turnSub.charAt(k);
             turnTimes = Character.getNumericValue(turnTimes);
             System.out.println("Turn times: " + turnTimes);
-
-            if(hasRing(s) && hasTwistUp(s)){
-                switch(getRing()){
-                    // turn array([ringnumber])
+        }
+    }
         
-                    case 1: turnCell1(turnTimes);
+
+            if(hasRing(s) && hasTwist(s)){
+                switch(getRing()){
+        
+                    case 1: turnRing(0, turnTimes, twistOption, turnTimeString);
                     break;
-                    case 2: turnCell2(turnTimes);
+                    case 2: turnRing(1, turnTimes, twistOption, turnTimeString);
                     break;
-                    case 3: turnCell3(turnTimes);
+                    case 3: turnRing(2, turnTimes, twistOption, turnTimeString);
                     break;
-                    case 4: turnCell4(turnTimes);                
+                    case 4: turnRing(3, turnTimes, twistOption, turnTimeString);                
                     break;
                     
                 }
-
+            }
+            
             
         }
 
-    }
-    
-    }
-       
-
-        
-        }
-
-  
-       
-   /*
-   public final boolean containsDigit(String s) {
-    boolean containsDigit = false;
-
-    if (s != null && !s.isEmpty()) {
-        for (char c : s.toCharArray()) {
-            if (containsDigit = Character.isDigit(c)) {
-                break;
-            }
-        }
-    }
-    
-    return containsDigit;
-}*/
-    
     int actualNumber = 0;
 
     public BicycleLock(){
@@ -283,7 +232,6 @@ class BicycleLock{
     public static void main(String...args){
         BicycleLock newLock = new BicycleLock();
         String userInput;
-        newLock.createMap();
 
         
 
@@ -297,14 +245,6 @@ class BicycleLock{
         System.out.println("unlocked");
 
         }
-        
- 
-        /*
-        Ring 1: Twist up 4 times 
-        Ring 3: Twist down once
-            Ring 4: Twist up or down 5 times
-*/
-        
     }
 
 
